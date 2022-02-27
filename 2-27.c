c247bc69e24ba63348486eb1b788465d3069abe5
zgwaiwyd <99521061+zgwaiwyd@users.noreply.github.com>
2022-02-27 22:56:39 +0800

    Add files via upload

---
+++ 2-27.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include<math.h>
+#include<stdlib.h>
+//int main()
+//
+//{
+//	int k = 0;
+//	static char st[] = "c    language";
+//	k = strlen(st);
+//	printf("%d\n", k);
+//	return 0;
+//}
+//void main()
+//{
+//	static char st1[30] = "My name is ";
+//	int st2[10];
+//	printf("input your name:\n");
+//	gets(st2);
+//	strcat(st1, st2);
+//	puts(st1);
+//		return 0;
+//}
+//void main()
+//
+//{
+//	int k=0;
+//	static char st1[15], st2[] = "c language";
+//	printf("input a string:");
+//	gets(st1);
+//	k = strcmp(st1, st2);
+//	if (k == 0)printf("st1=st2\n");
+//	if (k > 0)printf("st1>st2\n");
+//	if (k < 0)printf("st1<st2\n");
+//	return 0;
+//}
